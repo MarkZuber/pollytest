0580a7c0dcc51a888ca0ddc8ea09dc9fc27db389
Mark Zuber <mzuber@microsoft.com>
2018-01-22 12:19:23 -0800

    update to java9

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6,9 +6,9 @@ sudo apt install -y doxygen graphviz cmake
 # install oracle java 8 (will need to accept license during this)
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install java-common oracle-java8-installer
+sudo apt-get install java-common oracle-java9-installer
 
-sudo apt-get install oracle-java8-set-default
+sudo apt-get install oracle-java9-set-default
 source /etc/profile
 
 # install android studio
